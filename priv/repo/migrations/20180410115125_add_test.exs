d0936849914e350d20123d5ce740bf99ba5e3978
uniqueshiv <uniqueshivchauhan@gmail.com>
2018-04-10 17:22:51 +0530

    initi

---
+++ priv/repo/migrations/20180410115125_add_test.exs
@@ -0,0 +1,11 @@
+defmodule Hello.Repo.Migrations.AddTest do
+  use Ecto.Migration
+
+  def change do
+    create table(:test) do
+      add :title, :string
+
+      timestamps()
+    end
+  end
+end
